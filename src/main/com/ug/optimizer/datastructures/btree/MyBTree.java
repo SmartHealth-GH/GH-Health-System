beb4ed44538b83aeb7980502c89bf2049e17aea4
Emmanuel Addo <eaddo078@st.ug.edu.gh>
2026-08-09 21:08:20 +0000

    feat: implement B-tree insert, search, delete, and traverse

--- src/main/com/ug/optimizer/datastructures/btree/MyBTree.java
+++ src/main/com/ug/optimizer/datastructures/btree/MyBTree.java
@@ -3,13 +3,20 @@ package main.com.ug.optimizer.datastructures.btree;
 public class MyBTree {
 
     private BTreeNode root;
-    private int order;
+    private final int order;
+    private final int t;
     private int size;
 
     public MyBTree(int order) {
+        if (order < 4 || order % 2 != 0) {
+            throw new IllegalArgumentException("order must be an even integer >= 4");
+        }
         this.order = order;
+
+        this.t = order / 2;
         this.root = new BTreeNode(order, true);
         this.size = 0;
+
     }
 
     public BTreeNode getRoot() {
@@ -29,15 +36,39 @@ public class MyBTree {
     }
 
     public void insert(int key) {
+        BTreeNode r = root;
+        if (r.getNumKeys() == 2 * t - 1) {
+            BTreeNode s = new BTreeNode(order, false);
+            s.getChildren()[0] = r;
+            s.splitChild(0, r);
+            int i = (s.getKeys()[0] < key) ? 1 : 0;
+            s.getChildren()[i].insertNonFull(key);
+            root = s;
+        } else {
+            r.insertNonFull(key);
+        }
+        size++;
     }
 
     public boolean search(int key) {
-        return false;
+        return root.search(key) != null;
     }
 
     public void delete(int key) {
+        if (!search(key)) {
+            return;
+        }
+        root.remove(key);
+        if (root.getNumKeys() == 0 && !root.isLeaf()) {
+            root = root.getChildren()[0];
+        }
+        size--;
     }
 
     public void traverse() {
+        if (root.getNumKeys() > 0) {
+            root.traverse();
+        }
+        System.out.println();
     }
 }
